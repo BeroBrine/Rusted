3672fd125fc75eca9379b91eb00cae9b643f0217
BeroBrine <abhishekrana8818@gmail.com>
2025-02-17 13:37:31 +0530

    feat: status line added

--- src/editor/main_editor.rs
+++ src/editor/main_editor.rs
@@ -1,9 +1,10 @@
 use std::io::{stdout, Stdout, Write};
 
 use crossterm::{
-    cursor::MoveTo,
+    cursor::{self, MoveTo},
     event::{self, read},
-    style, terminal, ExecutableCommand, QueueableCommand,
+    style::{self, Color, Stylize},
+    terminal, ExecutableCommand, QueueableCommand,
 };
 
 use super::action::Action;
@@ -15,6 +16,14 @@ pub struct Editor {
     size: (u16, u16),
     cx: u16,
     cy: u16,
+}
+
+impl Drop for Editor {
+    fn drop(&mut self) {
+        let _ = self.stdout.flush();
+        let _ = self.stdout.execute(terminal::LeaveAlternateScreen);
+        let _ = terminal::disable_raw_mode();
+    }
 }
 
 impl Editor {
@@ -40,9 +49,59 @@ impl Editor {
     }
 
     fn draw_statusline(&mut self) -> anyhow::Result<()> {
-        println!("the size is {:?}" ,self.cx); 
-        self.stdout.execute(MoveTo(0,  self.size.1 - 2))?;
-        self.stdout.queue(style::Print("Status Line"))?;
+        self.stdout.execute(MoveTo(0, self.size.1 - 2))?;
+        let mode = self.get_mode().to_uppercase();
+        let pos = format!(" {}:{} ", self.cx, self.cy);
+        let file = format!(" src/main.rs");
+        let file_width = self.size.0 as usize - mode.len() - pos.len() - 2; // -2 for the
+                                                                            // seperators in mode
+        self.stdout
+            .queue(style::PrintStyledContent("█".with(Color::Rgb {
+                r: 184,
+                g: 144,
+                b: 243,
+            })))?;
+        self.stdout.queue(style::PrintStyledContent(
+            mode.with(Color::Rgb { r: 0, g: 0, b: 0 }).on(Color::Rgb {
+                r: 184,
+                g: 144,
+                b: 243,
+            }),
+        ))?;
+        self.stdout.queue(style::PrintStyledContent(
+            "█"
+                .with(Color::Rgb {
+                    r: 184,
+                    g: 144,
+                    b: 243,
+                })
+                .on(Color::Rgb {
+                    r: 255,
+                    g: 255,
+                    b: 255,
+                }),
+        ))?;
+        self.stdout.queue(style::PrintStyledContent(
+            format!("{:<width$}", file, width = file_width as usize)
+                .with(Color::Rgb {
+                    r: 255,
+                    g: 255,
+                    b: 255,
+                })
+                .on(Color::Rgb {
+                    r: 128,
+                    g: 128,
+                    b: 128,
+                }),
+        ))?;
+
+        self.stdout.queue(style::PrintStyledContent(
+            pos.with(Color::Rgb { r: 0, g: 0, b: 0 }).on(Color::Rgb {
+                r: 184,
+                g: 144,
+                b: 243,
+            }),
+        ))?;
         Ok(())
     }
 
@@ -77,20 +136,20 @@ impl Editor {
             };
         }
 
-        self.stdout.execute(terminal::LeaveAlternateScreen)?;
-        terminal::disable_raw_mode()?;
-
         Ok(())
     }
 
     fn handle_event(&mut self, event: event::Event) -> anyhow::Result<Option<Action>> {
+        if matches!(event, event::Event::Resize(_, _)) {
+            self.size = terminal::size()?;
+        }
         match self.mode {
             Mode::Normal => self.handle_normal_mode(event),
             Mode::Insert => self.handle_insert_mode(event),
         }
     }
 
-    fn handle_normal_mode(&self, event: event::Event) -> anyhow::Result<Option<Action>> {
+    fn handle_normal_mode(&mut self, event: event::Event) -> anyhow::Result<Option<Action>> {
         match event {
             event::Event::Key(ev) => match ev.code {
                 event::KeyCode::Char('q') => Ok(Some(Action::Quit)),
@@ -98,7 +157,10 @@ impl Editor {
                 event::KeyCode::Char('j') | event::KeyCode::Down => Ok(Some(Action::MoveDown)),
                 event::KeyCode::Char('k') | event::KeyCode::Up => Ok(Some(Action::MoveUp)),
                 event::KeyCode::Char('l') | event::KeyCode::Right => Ok(Some(Action::MoveRight)),
-                event::KeyCode::Char('i') => Ok(Some(Action::EnterMode(Mode::Insert))),
+                event::KeyCode::Char('i') => {
+                    self.stdout.execute(cursor::EnableBlinking)?;
+                    Ok(Some(Action::EnterMode(Mode::Insert)))
+                }
 
                 _ => Ok(None),
             },
@@ -110,7 +172,10 @@ impl Editor {
     fn handle_insert_mode(&mut self, event: event::Event) -> anyhow::Result<Option<Action>> {
         match event {
             event::Event::Key(key) => match key.code {
-                event::KeyCode::Esc => Ok(Some(Action::EnterMode(Mode::Normal))),
+                event::KeyCode::Esc => {
+                    self.stdout.execute(cursor::DisableBlinking)?;
+                    Ok(Some(Action::EnterMode(Mode::Normal)))
+                }
                 event::KeyCode::Char(c) => {
                     self.stdout.queue(style::Print(c))?;
                     self.cx += 1;
@@ -119,6 +184,13 @@ impl Editor {
                 _ => Ok(None),
             },
             _ => Ok(None),
+        }
+    }
+
+    fn get_mode(&self) -> String {
+        match self.mode {
+            Mode::Insert => String::from("Insert"),
+            Mode::Normal => String::from("Normal"),
         }
     }
 }
